fd0ed4b509ea08e0e2d939e3fa78b6407fb50ff8
Blaine Kasten <blaine@macpractice.net>
2013-07-08 08:20:25 -0500

    Build Files.

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+autoWatch = false;
+
+basePath = '';
+
+frameworks = ['jasmine'];
+
+browsers = ['Safari', 'Firefox']//, 'PhantomJS'];
+  /*
+    Chrome
+    ChromeCanary
+    Firefox
+    Opera
+    Safari
+    PhantomJS
+  */
+//captureTimeout = 60000,
+
+colors = true;
+
+//exclude = [],
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  //'src/**/*.coffee',
+  'libs/*.js',
+  'tests/*.coffee'
+];
+
+
+//hostname = 'localhost',
+//logLevel = 'LOG_INFO',
+  /* LOG_DISABLE LOG_ERROR LOG_WARN LOG_INFO LOG_DEBUG */
+//loggers = [{type = 'console'}],
+
+port = 9876;
+
+preprocessers = { '**/*.coffee': 'coffee' };
+
+//proxies = {},
+//reportSlowerThan = 200;
+
+reporters = ['dots']; /* dots progress junit growl coverage */
+
+runnerPort = 9100;
+singleRun = false;
+//urlRoot = ''
+
+
